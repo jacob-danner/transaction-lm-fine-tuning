4535b579902221b3acd68e28373d620efff7885d
Jacob Danner <jacob@jacobdanner.dev>
2025-03-16 13:44:31 -0500

    feat: simplify gpt 1 causal fine tune by putting in notebooks

--- src/shared/config.py
+++ src/shared/config.py
@@ -5,7 +5,6 @@ class Settings(BaseSettings):
     hf_user_name: str
     hf_dataset_repo_name: str
     gpt_1_causal_finetune: str
-    base_training_output_dir: str
     device: str
 
     class Config:

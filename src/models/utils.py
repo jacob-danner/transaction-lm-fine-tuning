291cde3d25160032080dc859a664f03c6e1541dc
Jacob Danner <78774495+jacob-danner@users.noreply.github.com>
2025-03-16 13:54:40 -0500

    feat: simplify by using notebooks (#1)
    * feat: replace upload script with notebook
    
    * feat: simplify gpt 1 causal fine tune by putting in notebooks

--- src/models/utils.py
+++
@@ -1,6 +0,0 @@
-from src.shared.config import settings
-from datasets import load_dataset
-
-
-def transactions_dataset_init():
-    return load_dataset(f"{settings.hf_user_name}/{settings.hf_dataset_repo_name}")

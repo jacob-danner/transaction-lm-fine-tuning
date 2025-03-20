b4f0f1989a17b9952700f3b78c437901da2ed90b
Jacob Danner <jacob@jacobdanner.dev>
2025-03-19 19:45:37 -0500

    wip: gemma lora

--- src/shared/config.py
+++ src/shared/config.py
@@ -5,6 +5,8 @@ class Settings(BaseSettings):
     hf_user_name: str
     hf_dataset_repo_name: str
     gpt_1_causal_finetune: str
+    gemma_3_1b_causal_finetune: str
+    gemma_3_1b_causal_finetune_lora: str
     device: str
 
     class Config:

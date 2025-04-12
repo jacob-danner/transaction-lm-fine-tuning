04e540e43ce500d28df050e55ecb5f36d43f6aad
Jacob Danner <jacob@jacobdanner.dev>
2025-04-12 15:00:47 -0500

    fix: config

--- src/shared/config.py
+++ src/shared/config.py
@@ -5,9 +5,9 @@ class Settings(BaseSettings):
     hf_user_name: str
     hf_dataset_repo_name: str
     gpt_1_causal_finetune: str
+    gpt_1_sequence_classification: str
     gemma_3_1b_causal_finetune: str
     gemma_3_1b_causal_finetune_lora: str
-    gpt_1_sequence_classification: str
     device: str
 
     class Config:
